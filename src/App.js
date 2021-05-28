dc4c366005d79add0cbc3cbd445d7352ce12920b
Gustavo Lago <84886995+etmaduckso@users.noreply.github.com>
2021-05-28 04:11:30 -0300

    book.6 erro no onSubmit

--- src/App.js
+++ src/App.js
@@ -3,48 +3,53 @@ import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
+  const [book, setBook] = useState("");
+  const [result, setResult] = useState([]);
+  const [apiKey, setApiKey] = useState(
+    "AIzaSyBd9H4JeRLdn_QE28OduTsOfCNJdJ19IM0");
 
 
-  const [book,setBook] = useState("");
-  const [result,setResult] = useState([]);
-  const [apiKey,setApiKey] = useState("AIzaSyBd9H4JeRLdn_QE28OduTsOfCNJdJ19IM0");
-  
-  
-  function handleChange(event){
-  
+    function handleChange(event) {
     const book = event.target.value;
     setBook(book);
   }
-  
-  function handleSubmit(event){
-  
+
+  function handleSubmit(event) {
     event.preventDefault();
-      Axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=30").then(data => {
+    Axios.get(
+      "https://www.googleapis.com/books/v1/volumes?q=" +
+        book +
+        "&key=" +
+        apiKey +
+        "&maxResults=30"
+    ).then((data) => {
       console.log(data.data.items);
       setResult(data.data.items);
-      })
+    });
   }
-  
-    return (
-  <div className={handleSubmit}>
-  
-  <h1>+ LIVROS</h1>
-  
+
+  return (
+    <div className={handleSubmit}>
+      <h1>+ LIVROS</h1>
+
       <form onSubmit="handleSubmit">
-               <div className="form-group">
-        
-                <input type="text" onChange={handleChange} className="form-control mt-10" placeholder="Search for Books" autoComplete="off"/>
-  
-              </div>
-              <button type="submit" className="btn btn-danger">Search</button>
-  
+        <div className="form-group">
+          <input
+            type="text"
+            onChange={handleChange}
+            className="form-control mt-10"
+            placeholder="Qual Livro Deseja?"
+            autoComplete="off"
+          />
+        </div>
+        <button type="submit" className="btn btn-danger">
+          Encontrar!
+        </button>
       </form>
-  
-  
-  
 
-
-
-</div>
+      {result.map(book =>(
+<image src={book.volumesInfo.imageLinks.thumbnail} alt={book.title}/>
+      ))}
+    </div>
   );
 }
