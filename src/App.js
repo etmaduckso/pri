e1c774b840068514db4cf95e345e3519522c5e6a
Gustavo Lago <84886995+etmaduckso@users.noreply.github.com>
2021-05-28 02:47:53 -0300

    book1 projeto book

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
+import Axios from "axios";
 import { useState } from "react";
 import "./styles.css";
 
 export default function App() {
 
 
-
-
-
-
   const [Books,setBook] = useState("");
   const [result,setResult] = useState([]);
+  const [apiKey,setApiKey] = useState("AIzaSyAk6-GfKEDjnsCIp0vrK3psAt_vrSKb61E");
   
   
   function handleChange(event){
   
-  
+    const book = event.target.value;
+    setBook(book);
   }
   
   function handleSubmit(event){
   
-  
-  
+    event.preventDefault();
+      Axios.get("https://www.googleapis.com/books/v1/volumes?q="+book+"&key="+apiKey+"&maxResults=40").then(data => {
+console.log(data);
+      })
   }
   
     return (
@@ -29,7 +30,7 @@ export default function App() {
   <h1>+ LIVROS</h1>
   
       <form onSubmit="handleSubmit">
-               <div class="form-group">
+               <div className="form-group">
         
                 <input type="text" onChange={handleChange} className="form-control mt-10" placeholder="Search for Books" autoComplete="off"/>
   
